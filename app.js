9f48c51705879201a8e00d33630fe3f676688115
Obscur1t <151863229+Obscur1t@users.noreply.github.com>
2024-10-10 21:13:34 +0500

    Add change them for header

--- app.js
+++ app.js
@@ -5,13 +5,38 @@ let userName;
 
 regBtn.addEventListener('click', () => {
     userName = prompt('What is your name');
-    heroName.textContent = `Hi, ${userName}`
+    heroName.textContent = `Hi, ${userName}`;
 })
 
 secBtn.addEventListener('click', () => {
-    alert(`This button doesn't work right now`)
+    alert(`This button doesn't work right now`);
 })
 
+////////////////////Change theme
+
+const changeBtn = document.querySelector('#sun-btn');
+const darkHeader = document.querySelector('#dark-theme-bc-header');
+const darkTech = document.querySelector('#tech-id');
+const darkProject = document.querySelector('#project-id');
+const darkFooter = document.querySelector('#contacts');
+const darkLogo = document.querySelector('.logo');
+const darkHeaderCl = document.querySelector('#dark-header-cl');
+
+
+changeBtn.addEventListener('click', () => {
+    darkHeader.classList.toggle('light-bc-header');
+    darkTech.classList.toggle('light-bc-tech');
+    darkProject.classList.toggle('light-bc-project');
+    darkFooter.classList.toggle('light-bc-footer');
+    changeBtn.classList.toggle('light-changeBtn');
+    darkLogo.classList.toggle('light-header-logo');
+    darkHeaderCl.classList.toggle('light-header-cl');
+    changeBtn.classList.toggle('light-change-btn');
+    regBtn.classList.toggle('light-btn');
+    secBtn.classList.toggle('light-btn')
+})
+
+console.log('Hi');
 
 
 
